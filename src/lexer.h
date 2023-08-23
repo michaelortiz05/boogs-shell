8482c00f3ca35aa41356f0170fd452567db53adb
Michael Ortiz <michaelortiz05@hotmail.com>
2023-08-22 20:54:34 -0500

    refactored files

---
+++ src/lexer.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+typedef enum {COMMAND, ARGUMENT, REDIRECTION, PIPE, SPECIAL} TokenType;
+typedef enum {START, IN_WORD, IN_QUOTE} TokenizerState;
+
+typedef struct {
+    char* value;
+    TokenType type;
+} Token;
+
+typedef struct {
+    Token* tokens;
+    int count;
+} TokenList;
+
+int tokenizer(const char* input, TokenList* result);
+void printTokens(TokenList *tokenList);
+const char* tokenTypeToString(TokenType type);
